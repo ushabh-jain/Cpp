2aa00da58147ad6080e43d1bb236b4ace88f63de
ushabh-jain <ushabhjain07@gmail.com>
2025-01-11 22:07:25 +0530

    second commit

--- basics.cpp
+++ basics.cpp
@@ -6,17 +6,101 @@
 using namespace std;
 //this is the main function
 int main(){
-    int age = 25; //4 byte
-    char grade = 'a'; //1 byte
-    float pi = 3.14f; //4 byte
-    bool isValid = true; //1 byte
-    double price = 100.99; //8 byte;
 
-    cout<<age<<endl;
-    cout<<grade<<endl;
-    cout<<pi<<endl;
-    cout<<isValid<<endl;
-    cout<<price<<endl;
+    //DATA TYPES AND VARIABLES
+
+
+    // int age = 25; //4 byte
+    // char grade = 'a'; //1 byte
+    // float pi = 3.14f; //4 byte
+    // bool isValid = true; //1 byte
+    // double price = 100.99; //8 byte;
+    // int newprice = (int)price; // this is type casting
+
+    // cout<<age<<endl;
+    // cout<<grade<<endl;
+    // cout<<pi<<endl;
+    // cout<<isValid<<endl;
+    // cout<<price<<endl;
+    // cout<<newprice<<endl;
+
+    //USER INPUT
+
+    // int marks;
+    // cout<<"enter your marks : ";
+    // cin>> marks;
+    // cout<<"your marks :"<<marks<<endl;
+    
+
+    //INCREMENT DECREMENT
+
+    // int a = 10;
+    // int d = 11;
+    // int b = a++; // kaam hoga pehele fir update
+    // int c = ++d; // update hoga pehele fir kaam
+    // cout<<b<<endl;
+    // cout<<c;
+
+    //CONDITIONS
+
+
+    // int age;
+    // cout<<"age daal : ";
+    // cin>>age;
+    // if(age>=18){
+    //     cout<<"vote pa";
+    // }
+    // else{
+    //     cout<<"fer kaddi";
+    // }
+
+    //LOOPS
+
+    // int n;
+    // cout<<"number daal : ";
+    // cin>>n;
+    // int sum=0;
+    // // while(n<10){
+    // //     cout<<"LA LA LA";
+    // // }
+    // for(int i=0;i<=n;i++){
+    //     sum+=i;
+       
+    // }
+    // cout<<sum;
+
+    //Q1 Generate first n Prime numbers 
+
+  
+
+// Function to check if a number is prime
+// bool isPrime(int num) {
+//     if (num <= 1) return false;
+//     for (int i = 2; i * i <= num; i++) {
+//         if (num % i == 0) return false;
+//     }
+//     return true;
+// }
+
+// int main() {
+//     int m, n;
+//     cin >> n >> m; // Input limit (n) and number of primes to generate (m)
+
+//     int primeCount = 0; // Counter for the number of primes found
+
+//     for (int i = 2; i <= n; i++) {
+//         if (isPrime(i)) {
+//             cout << i << endl; // Print the prime number
+//             primeCount++;
+//             if (primeCount == m) break; // Stop when m primes are found
+//         }
+//     }
+
+
+
+
+  
+
 
 
     return 0;
